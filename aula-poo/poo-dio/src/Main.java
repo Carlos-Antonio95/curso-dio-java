58012b441adf07b42c29913c7272e8bc57e79e9e
Carlos Antonio <carlosantonioob@gmail.com>
2025-03-08 08:12:03 -0300

    Aula poo dio

---
+++ aula-poo/poo-dio/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)  {
+        var male = new Person();
+        male.name = "Jõao";
+        male.age = 12;
+        var famale = new Person();
+        famale.name = "Maria";
+        famale.age = 10;
+        System.out.println();
+        System.out.println();
+    }
+}
